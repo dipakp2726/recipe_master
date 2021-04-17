9a8d8aa9b5fd52dc3e25efb9ced81803fc375be1
dipakp2726 <dipakp2726@gmail.com>
2021-04-17 11:56:18 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'model/model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Recipe Master',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  File _image;
+  final picker = ImagePicker();
+
+  @override
+  void initState() async {
+    super.initState();
+
+    await loadModel();
+  }
+
+  @override
+  Future<void> dispose() async {
+    await Tflite.close();
+    super.dispose();
+  }
+
+  Future<String> loadModel() async {
+    return Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+      useGpuDelegate: true,
+    );
+  }
+
+  Future getImage(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: source);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(
+                    _image,
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => getImage(ImageSource.camera),
+              child: Text('select image from camera'),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => getImage(ImageSource.gallery),
+              child: Text('select image from gallery'),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: _getFab(),
+    );
+  }
+
+  _getFab() {
+    if (_image != null) {
+      return FloatingActionButton.extended(
+        onPressed: () async {
+          Tflite.runModelOnImage(path: _image.path, asynch: true)
+              .then((value) async {
+            print(value);
+
+            if (value.length > 0) {
+              final Map itemName = value.first;
+
+              final item = recipeList
+                  .where((element) => element.name == itemName['label']);
+
+              print(item);
+
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => Result(item: item.first)));
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('No item found'),
+              ));
+            }
+          });
+        },
+        label: Text('get recipe'),
+      );
+    }
+  }
+}
+
+class Result extends StatelessWidget {
+  Result({Key key, @required this.item}) : super(key: key);
+
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Center(
+                child: Text(
+                  'Item name : ${item.name}',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              ExpansionTile(
+                title: Text('ingredients'),
+                children: [Text(item.ingredients1)],
+              ),
+              ExpansionTile(
+                title: Text('Recipe'),
+                children: [Text(item.recipe1)],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/model.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class Item {
+  String name;
+  String ingredients;
+  String diet;
+  String prepTime;
+  String cookTime;
+  String flavorProfile;
+  String course;
+  String state;
+  String region;
+  String comment;
+  String ingredients1;
+  String recipe1;
+  String recipe2;
+  String recipe3;
+  String nutritions;
+
+  Item(
+      {this.name,
+      this.ingredients,
+      this.diet,
+      this.prepTime,
+      this.cookTime,
+      this.flavorProfile,
+      this.course,
+      this.state,
+      this.region,
+      this.comment,
+      this.ingredients1,
+      this.recipe1,
+      this.recipe2,
+      this.recipe3,
+      this.nutritions});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    ingredients = json['ingredients'];
+    diet = json['diet'];
+    prepTime = json['prep_time'];
+    cookTime = json['cook_time'];
+    flavorProfile = json['flavor_profile'];
+    course = json['course'];
+    state = json['state'];
+    region = json['region'];
+    comment = json['comment'];
+    ingredients1 = json['ingredients__1'];
+    recipe1 = json['recipe1'];
+    recipe2 = json['recipe2'];
+    recipe3 = json['recipe3'];
+    nutritions = json['Nutritions'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['ingredients'] = this.ingredients;
+    data['diet'] = this.diet;
+    data['prep_time'] = this.prepTime;
+    data['cook_time'] = this.cookTime;
+    data['flavor_profile'] = this.flavorProfile;
+    data['course'] = this.course;
+    data['state'] = this.state;
+    data['region'] = this.region;
+    data['comment'] = this.comment;
+    data['ingredients__1'] = this.ingredients1;
+    data['recipe1'] = this.recipe1;
+    data['recipe2'] = this.recipe2;
+    data['recipe3'] = this.recipe3;
+    data['Nutritions'] = this.nutritions;
+    return data;
+  }
+
+  List<Item> fromMapList(List<dynamic> dynamicDataList) {
+    final List<Item> ItemList = <Item>[];
+
+    if (dynamicDataList != null) {
+      for (dynamic dynamicData in dynamicDataList) {
+        if (dynamicData != null) {
+          ItemList.add(Item.fromJson(dynamicData));
+        }
+      }
+    }
+    return ItemList;
+  }
+
+  List<Map<String, dynamic>> toMapList(List<Item> objectList) {
+    final List<Map<String, dynamic>> mapList = <Map<String, dynamic>>[];
+    if (objectList != null) {
+      for (Item data in objectList) {
+        if (data != null) {
+          mapList.add(data.toJson());
+        }
+      }
+    }
+    return mapList;
+  }
+}
